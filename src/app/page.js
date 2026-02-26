b9b046a88fedb8b271f85d1a709badb1218a0111
Vishal Kumar <visalkr976@gmail.com>
2026-02-26 15:16:56 +0530

    updated added a sticky image

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,13 @@
-import Navbar    from '@/components/Navbar/Navbar'
+
 import HeroSlider from '@/components/HeroSlider/HeroSlider'
-import WhyUs      from '@/components/WhyUs/Whyus'
-import Process    from '@/components/Process/Process'
+import WhyUs from '@/components/Whyus/Whyus'
+import Process from '@/components/Process/Process'
 import StickyCanvas from '@/components/StickyCanvas/StickyCanvas'
 
 export default function HomePage() {
   return (
     <main>
-      <Navbar />
-      <HeroSlider />
+      <HeroSlider/>
 
       <StickyCanvas
         image="https://images.unsplash.com/photo-1509391366360-2e959784a276?q=80&w=2072"
